c0f5ef5b509c283d08f51671b25b024500bbc330
zithern-git <2385186645@qq.com>
2026-03-21 11:23:48 +0800

    左侧菜单的静态的搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,6 +3,53 @@
     <!-- 左侧菜单 -->
     <div class="layout_slider">
       <Logo />
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单组件 -->
+        <el-menu
+          active-text-color="#ffd04b"
+          background-color="rgb(150, 194, 227)"
+          class="el-menu-vertical-demo"
+          default-active="1"
+          text-color="#fff"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-menu-item index="1">
+            <el-icon><HomeFilled /></el-icon>
+            <span>首页</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <el-icon><DataBoard /></el-icon>
+            <span>数据大屏</span>
+          </el-menu-item>
+          <el-sub-menu index="3">
+            <template #title>
+              <el-icon><Lock /></el-icon>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-1">用户管理</el-menu-item>
+            <el-menu-item index="1-2">角色管理</el-menu-item>
+            <el-menu-item index="1-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="4">
+            <template #title>
+              <el-icon><User /></el-icon>
+              <span>系统管理</span>
+            </template>
+            <el-menu-item-group title="Group One">
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title>item four</template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航 -->
     <div class="layout_tabbar">tabbar</div>
@@ -13,6 +60,15 @@
 
 <script setup lang="ts">
 import Logo from './logo/index.vue'
+
+import { HomeFilled, DataBoard, Lock, User } from '@element-plus/icons-vue'
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 </script>
 
 <style scoped lang="scss">
@@ -35,13 +91,17 @@ import Logo from './logo/index.vue'
   .layout_main {
     position: absolute;
     width: calc(100% - #{$base-menu-width});
-    // height: calc(100vh - #{$base-tabbar-height});
-    height: 1000px;
+    height: calc(100vh - #{$base-tabbar-height});
+    // height: 1000px;
     top: $base-tabbar-height;
     left: $base-menu-width;
     padding: 20px;
     background-color: #fff;
     overflow: auto;
   }
+}
+
+.scrollbar {
+  height: calc(100% - #{$base-menu-logo-height});
 }
 </style>
