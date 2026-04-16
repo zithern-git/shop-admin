b125b0d627f0a1e708d071911fb0baa4bfd5d0fb
zithern-git <2385186645@qq.com>
2026-04-16 21:00:36 +0800

    完成首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="profile-container">
-    <h2>{{userStore.username}}</h2>
+  <el-card>
+    <div class="box">
+      <img :src="userStore.avatar" alt="" class="avatar" />
+      <div class="bottom">
+        <h3 class="title">{{ `${getTime()}好${userStore.username}` }}</h3>
+        <p class="subtitle">后台测试管理系统</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="welcome">
+    <svg-icon name="welcome" width="500px" height="500px"></svg-icon>
   </div>
 </template>
 
 <script setup lang="ts">
-// 引入组合式API函数之生命周期函数
-import { onMounted } from 'vue'
-// 获取用户仓库
-import useUserStore from '@/store/modules/user'
-const userStore = useUserStore()
-// 目前首页挂载完毕发请求获取用户信息
-onMounted(() => {
-  userStore.userInfo()
-})
+  import { getTime } from '@/utils/time'
+  // 引入用户仓库，获取当前用户的头像、昵称
+  import useUserStore from '@/store/modules/user'
 
+  const userStore = useUserStore()
 </script>
+
+<style scoped lang="scss">
+  .box {
+    display: flex;
+    align-items: center;
+    .avatar {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+    }
+    .bottom {
+      margin-left: 20px;
+      .title {
+        font-size: 30px;
+        font-weight: 900;
+        margin-bottom: 30px;
+      }
+      .subtitle {
+        font-size: 16px;
+        font-weight: 500;
+        font-style: italic;
+        color: rgb(91, 152, 152);
+      }
+    }
+  }
+
+  .welcome {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+</style>
