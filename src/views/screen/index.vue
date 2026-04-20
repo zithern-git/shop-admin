a7a2b2723422a71627323ffda6191a485eb8ad05
zithern-git <2385186645@qq.com>
2026-04-20 17:37:08 +0800

    利用echarts和阿里云 DataV 提供的 GeoJSON）动态加载中国地图数据

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -12,7 +12,10 @@
           <Sex class="sex"></Sex>
           <Age class="age"></Age>
         </div>
-        <div class="center"></div>
+        <div class="center">
+          <Map class="map"></Map>
+          <Line class="line"></Line>
+        </div>
         <div class="right"></div>
       </div>
     </div>
@@ -26,6 +29,8 @@
   import Tourist from './components/tourist/index.vue'
   import Sex from './components/sex/index.vue'
   import Age from './components/age/index.vue'
+  import Map from './components/map/index.vue'
+  import Line from './components/line/index.vue'
 
   const screen = ref<HTMLElement | null>(null)
 
@@ -93,10 +98,18 @@
         }
         .center {
           flex: 2;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          .map {
+            flex: 3;
+          }
+          .line {
+            flex: 1;
+          }
         }
         .right {
           flex: 1;
-          background-color: rgb(67, 177, 208);
         }
       }
     }
